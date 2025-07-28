9bf3cd92d906fa23ed99fbc70c6a0a311664c690
devskinshy <yyshgk12@gmail.com>
2025-07-28 23:14:29 +0900

    2-5

---
+++ src/class1/ClassStart1.java
@@ -0,0 +1,21 @@
+package class1;
+
+public class ClassStart1 {
+    public static void main(String[] args) {
+        String stutent1Name = "학생1";
+        int stutent1Age = 20;
+        int stutent1Grade = 1;
+
+        String stutent2Name = "학생2";
+        int stutent2Age = 21;
+        int stutent2Grade = 2;
+
+        String stutent3Name = "학생3";
+        int stutent3Age = 22;
+        int stutent3Grade = 3;
+
+        System.out.println("이름 : " + stutent1Name + " 나이 : " + stutent1Age + " 성적 : " + stutent1Grade);
+        System.out.println("이름 : " + stutent2Name + " 나이 : " + stutent2Age + " 성적 : " + stutent2Grade);
+        System.out.println("이름 : " + stutent3Name + " 나이 : " + stutent3Age + " 성적 : " + stutent3Grade);
+    }
+}
