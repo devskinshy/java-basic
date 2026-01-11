3fe9053a0011a7a357728334f708af446d817ce1
devskinshy <yyshgk12@gmail.com>
2026-01-11 23:46:54 +0900

    5-31

---
+++ src/construct/MemberConstruct.java
@@ -0,0 +1,15 @@
+package construct;
+
+public class MemberConstruct {
+    String name;
+    int age;
+    int grade;
+
+    MemberConstruct(String name, int age, int grade) {
+
+        System.out.println("Constructor called" + "name : " + name + " age : " + age + " grade : " + grade);
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+}
